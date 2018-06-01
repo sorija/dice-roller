0d62da324d177073ef010481cf1b0d2cb366ef1a
Joan <joan.gawelda@gmail.com>
2018-06-01 17:27:20 +0200

    Wire up modifier and tweak result display

--- src/components/DiceRollerApp.js
+++ src/components/DiceRollerApp.js
@@ -6,28 +6,38 @@ class DiceRollerApp extends React.Component {
   state = {
     dieType: [4,6,8,10,12,20],
     modifiers: [-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10],
-    value: 0,
+    modValue: 0,
     diePick: undefined,
-    result: undefined
+    rollResult: undefined,
   };
   handleDiePick = (e) => {
     const value = e.target.value;
+    this.setState(() => ({diePick: value}));
+  };
+  handleModChange = (e) => {
+    const modValue = e.target.value;
+    this.setState(() => ({modValue: modValue}));
   };
   handleDieRoll = () => {
     const minDie = 1;
     const randomNum = Math.floor(Math.random() * (this.state.diePick - minDie + 1)) + minDie;
-    this.setState(() => ({result: randomNum}));
+    this.setState(() => ({rollResult: randomNum}));
   };
   render () {
     return (
-      <div>
+      <main>
         <h1>They see me rollin'</h1>
         <Die dieType={this.state.dieType} handleDiePick={this.handleDiePick}/>
-        <Modifier modifiers={this.state.modifiers} value={this.state.value}/>
-        <h3>Selected: {this.state.dieCount}D{this.state.diePick}</h3>
+        <Modifier modifiers={this.state.modifiers} modValue={this.state.modValue} handleModChange={this.handleModChange}/>
+        <h3>Selected:{this.state.diePick ? 'd' + this.state.diePick : ''}</h3>
         <button onClick={this.handleDieRoll}>Roll!</button>
-        <h2>You rolled {this.state.result}</h2>
-      </div>
+        {this.state.rollResult && (
+          <div>
+            <h2>Your result: {Number(this.state.rollResult) + Number(this.state.modValue)}</h2>
+            <h4>{this.state.rollResult}+{this.state.modValue > 0 ? this.state.modValue : '(' + this.state.modValue + ')'}</h4>
+          </div>
+        )}
+      </main>
     )
   }
 }
--- src/components/Modifier.js
+++ src/components/Modifier.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Modifier = (props) => (
   <div>
-    <select value={props.value} id="mod">
+    <select value={props.modValue} onChange={props.handleModChange} id="mod">
       {props.modifiers.map(function(mod,i) {
-        return <option key={i}>{mod}</option>
+        return <option value={mod} key={i}>{mod}</option>
       })}
     </select>
 </div>
