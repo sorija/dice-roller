9845c4413ae1aa93d9eee7c9616fa0314a018e80
Joan <joan.gawelda@gmail.com>
2018-07-12 15:58:51 +0300

    Rename dir per gh publishing requirements

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-console.log(path.join(__dirname, 'public'));
+console.log(path.join(__dirname, 'docs'));
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'docs'),
     filename: 'bundle.js'
   },
   module: {
@@ -23,6 +23,6 @@ module.exports = {
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, 'docs')
   }
 };
